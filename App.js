22385dd68f6c0289cd3ec040d7732a9d4f83e84a
kiet <buikiet16801@gmail.com>
2022-09-30 13:48:21 +0700

    upload

--- App.js
+++ App.js
@@ -2,21 +2,27 @@ import { StatusBar, SafeAreaView } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Router from './src/router';
 import { Entypo, AntDesign } from '@expo/vector-icons';
-import { Avatar } from "native-base";
+import { Avatar } from '@rneui/base';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar barStyle={'light-content'} />
       <View style={styles.header}>
-        <Entypo name="calendar" size={24} color="black" />
+        <View>
+          <Entypo name="calendar" size={24} color="black" />
+          <Text>T5, 29 tháng 9, 2022</Text>
+        </View>
         <View>
           <AntDesign name="search1" size={24} color="black" />
-          <Avatar bg="green.500" source={{
-            uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-          }}>
-            AJ
-          </Avatar>
+          <View>
+            <Avatar
+              size={64}
+              rounded
+              icon={{ name: 'pencil', type: 'font-awesome' }}
+              containerStyle={{ backgroundColor: '#6733b9' }}
+            />
+          </View>
         </View>
       </View>
       <Router />
@@ -31,8 +37,9 @@ const styles = StyleSheet.create({
   },
   header: {
     height: 100,
-    justifyContent:"space-between",
-    alignItems:"center",
-    backgroundColor:'#999'
+    display: 'flex',
+    justifyContent: "space-between",
+    alignItems: "center",
+    backgroundColor: '#999'
   }
 });
